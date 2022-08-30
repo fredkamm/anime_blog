a8d52156d54d44a7d12c018971f9630e79d03dd7
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-08-30 11:47:39 -0400

    sign/signup handlebars created. main handlebar started. signup/signin js started

--- models/Anime.js
+++ models/Anime.js
@@ -1,28 +1,28 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class Anime extends Model {}
+// class Anime extends Model {}
 
-Anime.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'anime',
-  }
-);
+// Anime.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     title: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'anime',
+//   }
+// );
 
-module.exports = Anime;
+// module.exports = Anime;
