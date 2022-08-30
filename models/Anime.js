40ccf9f268554e22cb96086097ec48f515d0af9c
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-08-30 14:29:04 -0400

    mainhandlebar update

--- models/Anime.js
+++ models/Anime.js
@@ -1,9 +1,7 @@
-// const { Model, DataTypes } = require('sequelize');
-// const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-// class Anime extends Model {}
-
-<<<<<<< HEAD
+class Anime extends Model {}
 Anime.init(
   {
     id: {
@@ -29,28 +27,5 @@ Anime.init(
     modelName: 'anime',
   }
 );
-=======
-// Anime.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     title: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//   },
-//   {
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'anime',
-//   }
-// );
->>>>>>> 7ce1a70e718cbc5a4e9f4b8d9a111dd8e84c1834
 
-// module.exports = Anime;
+module.exports = Anime;
