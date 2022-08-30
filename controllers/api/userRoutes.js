79e9259555c99237fb162e58c0a2a35ac6734f16
Humberto Rico <humbertorico@Humbertos-MacBook-Pro.local>
2022-08-30 10:33:01 -0400

    demo

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.get('/', async (req, res) => {
-
+    try{
+        
+    }    
 });
 
 router.post('/', async (req, res) => {
