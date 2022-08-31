f9e70b0415c823667132e131b77ed2118b4c6c3a
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-08-31 11:35:41 -0400

    homepage/sign/logout appear

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const { User} = require('../models');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  
+    res.render('homepage');
 });
 
 router.get('/post/:id', async (req, res) => {
@@ -11,11 +11,17 @@ router.get('/post/:id', async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-
+    // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signin');
 });
 
 router.get('/signup', (req, res) => {
-
+      // render the signup page when user clicks to sign up
+  res.render('signup');
 });
 
 module.exports = router;
